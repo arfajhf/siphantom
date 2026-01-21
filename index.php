fc234c1de32200260ed52f132cd595dcee41cde4
arfajhf <fjrhdytlh22@gmail.com>
2026-01-21 16:06:00 +0700

    halaman awal2

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] === true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIMACMUR - Sistem Monitoring Jamur</title>
+    <title>SiPhantom - Sistem Penyiraman Otomatis</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <style>
@@ -120,13 +120,12 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] === true) {
     <div class="landing-container">
         <!-- <div class="logo">🍄</div> -->
         <h1 class="title">SiPhantom</h1>
-        <p class="subtitle">Sistem Monitoring IoT</p>
+        <p class="subtitle">Sistem Penyiraman Otomatis</p>
         
         <a href="login.php" class="btn-login">
             🔐 Masuk ke Dashboard
         </a>
         
-
     </div>
     
     <div class="version">

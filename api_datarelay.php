f63dba3a41b77bbf59336fa0bd6360be382b090c
arfajhf <fjrhdytlh22@gmail.com>
2026-06-16 22:06:50 +0700

    access control api

--- api_datarelay.php
+++ api_datarelay.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: X-API-KEY, Content-Type");
+
 header('Content-Type: application/json');
 
 include 'koneksi.php'; 

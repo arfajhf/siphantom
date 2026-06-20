fbcb953d152f7e8b558bf638dc82aacf95fb709e
arfajhf <fjrhdytlh22@gmail.com>
2026-06-20 15:49:23 +0700

    deui we lah

--- api_datasensor.php
+++ api_datasensor.php
@@ -7,12 +7,15 @@ header('Content-Type: application/json');
 
 include 'koneksi.php'; 
 
-// mengambil data json dari relay
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 
-$query = "SELECT * FROM sensors WHERE kode_device = 'JAMUR395'";
+$query = "SELECT * FROM sensors WHERE kode_device = 'JAMUR395' ORDER BY timestamp DESC LIMIT 1";
 $result = $conn->query($query);
+
+if (!$result) {
+    die("Error Query: " . $conn->error);
+}
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();

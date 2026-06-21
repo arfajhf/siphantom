e533d6a3b815d69055e11920f6ae46cccc8e53c9
arfajhf <fjrhdytlh22@gmail.com>
2026-06-22 01:19:19 +0700

    dfg

--- api_reset_mode.php
+++ api_reset_mode.php
@@ -1,8 +1,8 @@
 <?php
 // Tambahin ini di baris paling atas
 // header("Access-Control-Allow-Origin: *"); 
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, X-API-KEY");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type, X-API-KEY");
 
 // // Kalau request-nya OPTIONS (preflight), langsung exit
 // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { exit; }
